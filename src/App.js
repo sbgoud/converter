5797af9e5f2f6c3a90929001c62fcbdf279a2cbf
Shashank Bandari <shashank2bandari@gmail.com>
2022-06-26 22:54:42 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import * as React from "react";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
+
+export default function RadioButtonsGroup() {
+  return (
+    <FormControl>
+      <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
+      <RadioGroup
+        aria-labelledby="demo-radio-buttons-group-label"
+        defaultValue="female"
+        name="radio-buttons-group"
+      >
+        <FormControlLabel value="female" control={<Radio />} label="Female" />
+        <FormControlLabel value="male" control={<Radio />} label="Male" />
+        <FormControlLabel value="other" control={<Radio />} label="Other" />
+        <FormControlLabel
+          value="viewer"
+          control={<Radio />}
+          label={
+            <div>
+              <strong>Viewer</strong><div>
+              Cannot do anything except view data</div>
+            </div>
+          }
+        />
+      </RadioGroup>
+    </FormControl>
+  );
+}
---
+++ src/App1.js
@@ -0,0 +1,56 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <body>
+       
+      <div class="total-box">
+        <div class="role">
+          <strong>Role</strong>
+        </div>
+        <div>
+          <div class="box-element1">
+            <input type="radio" id="radiobutton" name="rbutton"></input>
+            <label for="radiobutton">
+              <strong>Viewer</strong>
+              <div>Cannot do anything except view data</div>
+            </label>
+          </div>
+
+          <div class="box-element2">
+            <input type="radio" id="radiobutton2" name="rbutton"></input>
+            <label for="radiobutton2">
+              <strong>Editor</strong>
+              <div>Make changes to invoices, no payments</div>
+            </label>
+          </div>
+          <div class="box-element3">
+            <input type="radio" id="radiobutton3" name="rbutton"></input>
+            <label for="radiobutton3">
+              <strong>Controller</strong>
+              <div>All editor permissions, can make payments</div>
+            </label>
+          </div>
+          <div class="box-element4">
+            <input type="radio" id="radiobutton4" name="rbutton"></input>
+            <label for="radiobutton4">
+              <strong>Admin</strong>
+              <div>Full access, can add team members</div>
+            </label>
+          </div>
+
+          <div class="box-element5">
+            <a href="">Remove</a>
+          </div>
+
+          <div class="box-element6">
+            <a href="">Update permissions</a>
+          </div>
+        </div>
+      </div>
+    </body>
+  );
+}
+
+export default App;
