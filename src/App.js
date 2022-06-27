8266447af97c1e611f0dc7fd6368cc1c1beb81bb
Shashank Bandari <shashank2bandari@gmail.com>
2022-06-27 12:12:23 +0530

    added enter feets equivalent meters

--- src/App.js
+++ src/App.js
@@ -6,7 +6,14 @@ import "./App.css";
 export default function BasicButtons() {
   return (
     <div class="thebox">
-      <div class="heading"><strong>Feet to meter Converter</strong></div>
+      <div class="heading"><strong>Feets to meters Converter</strong></div>
+      <div>
+        Enter Feets = <span><input type="text"/>
+        </span>
+      </div>
+      <div>
+        Equivalent Meters = <button variant="Meters"> 0 Meters</button>
+      </div>
       <div class="stack">
       <Stack  direction="row">
         <Button variant="contained">Convert</Button>
