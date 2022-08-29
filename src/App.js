883b4c6cc9da48b315b336375b3c642375a6ad0e
Shashank Bandari <shashank2bandari@gmail.com>
2022-08-29 16:14:30 +0530

    changed heading to h2

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ export default function BasicButtons() {
 
   return (
     <div className="thebox">
-    <h3>Converter</h3>
+    <h2>Converter</h2>
       <div className="heading"><strong>Feets to meters Converter</strong></div>
       <div>
         Enter Feets =
